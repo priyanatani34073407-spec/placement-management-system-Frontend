1440cfe1241c722f82654506aab6695d710d60ba
Guna Sri Durgapriya Natani <priyanatani34073407@gmail.com>
2026-08-01 23:12:06 +0530

    Add ToastProvider around App component
    Wrap App component with ToastProvider for notifications.

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { ToastProvider } from "./components/Toast/ToastContext";
 
 createRoot(document.getElementById('root')).render(
   // safety to components
-  // enables to detect changes in URL and 
+  // enables to detect changes in URL and
   <BrowserRouter>
-    <App />
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </BrowserRouter>
 )
