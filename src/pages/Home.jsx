be2cc8389df41add9a83a3eaf5fdb91cc9e0b2bb
Guna Sri Durgapriya Natani <priyanatani34073407@gmail.com>
2026-08-08 22:02:05 +0530

    Refactor Home component formatting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import Dashboard from '../Dashboard/Dashboard';
-import Heading from '../Heading';
-function Home(){
-    return(
-        <div>
-            <Dashboard />
-            
-        </div>
-    )
-};
+
+function Home() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
 export default Home;
